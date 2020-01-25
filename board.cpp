7e1740d1283b632466078e28a9c03cd2c93b65d2
ADI13579 <45916688+ADI13579@users.noreply.github.com>
2020-01-25 20:56:39 +0545

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "board.h"
-
+#include "initialMenu.h"
 using namespace std;
 
-void setBoard(sf::RenderWindow &window)
+float scale = GetSystemMetrics(SM_CXSCREEN) / 1920.0;
+
+
+int main()
 {
-    window.clear();
-    board board;
-    board.backgroundCreate();
-    board.textFormat();
-    board.setBackground(window);
+    cout << scale;
+    sf::Music bckMusic;
+    bckMusic.openFromFile("musics/background.ogg");
+    bckMusic.setVolume(50);
+    bckMusic.play();
+
+
+    sf::RenderWindow window(sf::VideoMode(scale*1000, scale * 480), "Draught", sf::Style::Fullscreen);
+    window.setFramerateLimit(100);
+    sf::Event event;
+
+    setBoard(window);
+    initialMenu(window);
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.key.code == sf::Keyboard::Escape)
+            {
+                window.close();
+            }
+            
+        }
+    }
+    return 0;
 }
